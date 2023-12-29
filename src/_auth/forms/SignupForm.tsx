7352d2263e6e561a3f847520af5e7b038fee52c9
collins <colinsanisedo@gmail.com>
2023-12-29 21:00:14 +0100

    fix error

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -21,61 +21,64 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
 
-
-
-
- 
 const SignupForm = () => {
-  const { toast } = useToast()
-  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
-  const navigate = useNavigate();
+    const { toast } = useToast();
+    const navigate = useNavigate();
+    const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
   
-  const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
-
-
-  const { mutateAsync: signInAccount, isPending: isSigningIn } = useSignInAccount();
-
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof SignupValidation>>({
-    resolver: zodResolver(SignupValidation),
-    defaultValues: {
-      name: '',
-      username: '',
-      email: '',
-      password: '',
-    },
-  })
- 
-  // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    const newUser = await createUserAccount(values);
-
-    if (!newUser) {
-      return  toast({
-        title: 'Sign up failed. please try again.'
-    })
-  }
-
-   const session = await signInAccount({
-    email: values.email,
-    password: values.password,
-   })
-
-
-   if (!session) {
-    return toast({ title: 'Sign in failed.please try again.'})
-   }
-
-   const isLoggedIn = await checkAuthUser();
-
-   if(isLoggedIn) {
-    form.reset();
-
-    navigate('/')
-   } else {
-    return toast({ title: 'Sign in failed.please try again.'})
-   }
-}
+    const form = useForm<z.infer<typeof SignupValidation>>({
+      resolver: zodResolver(SignupValidation),
+      defaultValues: {
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+      },
+    });
+  
+    // Queries
+    const { mutateAsync: createUserAccount, isLoading: isCreatingAccount } = useCreateUserAccount();
+    const { mutateAsync: signInAccount, isLoading: isSigningInUser } = useSignInAccount();
+  
+    // Handler
+    const handleSignup = async (user: z.infer<typeof SignupValidation>) => {
+      try {
+        const newUser = await createUserAccount(user);
+  
+        if (!newUser) {
+          toast({ title: "Sign up failed. Please try again.", });
+          
+          return;
+        }
+  
+        const session = await signInAccount({
+          email: user.email,
+          password: user.password,
+        });
+  
+        if (!session) {
+          toast({ title: "Something went wrong. Please login your new account", });
+          
+          navigate("/sign-in");
+          
+          return;
+        }
+  
+        const isLoggedIn = await checkAuthUser();
+  
+        if (isLoggedIn) {
+          form.reset();
+  
+          navigate("/");
+        } else {
+          toast({ title: "Login failed. Please try again.", });
+          
+          return;
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    };
 
   return (
       <Form {...form}>
@@ -91,7 +94,7 @@ const SignupForm = () => {
           <p className="text-light-3 small-medium md:base-regular mt-2">To use Memories, please enter your details</p>
        
 
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-1 w-full mt-1">
+      <form onSubmit={form.handleSubmit(handleSignup)} className="flex flex-col gap-1 w-full mt-1">
         <FormField
           control={form.control}
           name="name"
